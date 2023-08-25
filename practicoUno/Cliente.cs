de07ab043428d218c6a120bb359263aad7367994
AngelMatiasA <angel.apaza010@gmail.com>
2023-08-25 19:23:53 -0300

    clase Cliente

--- practicoUno/Cliente.cs
+++ practicoUno/Cliente.cs
@@ -2,7 +2,14 @@ using System;
 namespace EspacioCliente; 
 
 public class Cliente
-{
-    
-    
+{ 
+    private string nombre; 
+    private string direccion; 
+    private string telefono;
+    private string DatosRefCliente;
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Telefono { get => telefono; set => telefono = value; }
+    public string Direccion { get => direccion; set => direccion = value; }
+    public string DatosRefCliente1 { get => DatosRefCliente; set => DatosRefCliente = value; }
 }
